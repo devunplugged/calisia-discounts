bb551d0a83afc841c4bdc4ab5e3051d82b08e723
devunplugged <66313338+devunplugged@users.noreply.github.com>
2021-05-07 14:21:11 +0200

    Create calisia-discounts.php
    version 0

---
+++ calisia-discounts.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Name: calisia-discounts
+ */
+
+
+
+
+## The following goes inside the constructor ##
+$calisiaDiscounts = new CalisiaDiscounts();
+// Simple, grouped and external products
+add_filter('woocommerce_product_get_price', array( $calisiaDiscounts, 'custom_price' ), 99, 2 );
+add_filter('woocommerce_product_get_regular_price', array( $calisiaDiscounts, 'custom_price' ), 99, 2 );
+// Variations 
+add_filter('woocommerce_product_variation_get_regular_price', array( $calisiaDiscounts, 'custom_price' ), 99, 2 );
+add_filter('woocommerce_product_variation_get_price', array( $calisiaDiscounts, 'custom_price' ), 99, 2 );
+
+// Variable (price range)
+add_filter('woocommerce_variation_prices_price', array( $calisiaDiscounts, 'custom_variable_price' ), 99, 3 );
+add_filter('woocommerce_variation_prices_regular_price', array( $calisiaDiscounts, 'custom_variable_price' ), 99, 3 );
+
+// Handling price caching (see explanations at the end)
+add_filter( 'woocommerce_get_variation_prices_hash', array( $calisiaDiscounts, 'add_price_multiplier_to_variation_prices_hash' ), 99, 3 );
+
+
+## This goes outside the constructor ##
+
+// Utility function to change the prices with a multiplier (number)
+class CalisiaDiscounts{
+    public function get_price_multiplier() {
+        return 0.9; // x2 for testing
+    }
+
+    public function custom_price( $price ) { //public function custom_price( $price, $product ) {
+        if(!$this->user_has_discount()){
+            return $price;
+        }
+
+        return (float) $price * $this->get_price_multiplier();
+    }
+
+    public function custom_variable_price( $price, $variation, $product ) {
+        if(!$this->user_has_discount()){
+            return $price;
+        }
+
+        return (float) $price * $this->get_price_multiplier();
+    }
+
+    public function add_price_multiplier_to_variation_prices_hash( $price_hash, $product, $for_display ) {
+        if(!$this->user_has_discount()){
+            return $price_hash; // return $price ??
+        }
+
+        $price_hash[] = $this->get_price_multiplier();
+        return $price_hash;
+    }
+
+    public function user_has_discount(){
+        
+        $user = wp_get_current_user();
+        if ( in_array( 'klient_ekskluzywny', (array) $user->roles ) ) {
+            return true;
+        }
+        return false;
+    }
+}
